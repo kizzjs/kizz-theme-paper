23600493d6a530733eb7d94e19fd286cf06918a4
Zeno Zeng <zenoofzeng@gmail.com>
2015-09-07 01:20:01 +0800

    update theme

--- theme/build.sh
+++
@@ -1,9 +0,0 @@
-stylus styles.styl
-cp styles.css ~/code/kizz-doc/public/static/
-
-coffee -o src/tmp/ -c src/*.coffee
-cp src/*.js src/tmp/
-browserify src/tmp/index.js > js/bundle-src.js
-uglifyjs js/bundle-src.js > js/bundle.js
-trash src/tmp
-cp js/bundle.js ~/code/kizz-doc/public/static/js/
